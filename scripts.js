91cd6e7dc554a26632ddbcc41f4ba8ab04057824
Ana Campbell <ana.campbell@descomplica.com.br>
2022-07-27 12:42:09 -0300

    Adds dynamic gifs

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 function checkTime(){
     let body = document.querySelector('body');
     let message = document.querySelector('.message');
+    let img = document.querySelector('img.photo');
 
-    let hours = new Date().getHours()
+    //let hours = new Date().getHours()
     let minutes = new Date().getMinutes()
-
+    hours = 16
     if (minutes < 10) {
         message.innerHTML = `It's ${hours}:0${minutes}`
     }
@@ -14,10 +15,13 @@ function checkTime(){
 
     if (hours>= 5 && hours < 12){
         body.style.backgroundImage = "url('assets/morning.png')";
+        img.src = "assets/morninggif.gif"
     } else if(hours >= 12 && hours <= 18){
         body.style.backgroundImage = "url('assets/afternoon.png')";
+        img.src = "assets/afternoongif.gif"
     } else {
         body.style.backgroundImage = "url('assets/night.png')";
+        img.src = "assets/nightgif.gif"
     }
     
 }
